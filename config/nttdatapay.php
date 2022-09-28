18b940bcac6deece4995cbe8528cdf039a0e5f2d
Rahul Reghunath <rahulreghunath11@gmail.com>
2022-09-28 17:02:23 +0530

    Transaction status function

--- config/nttdatapay.php
+++ config/nttdatapay.php
@@ -4,6 +4,7 @@ return array(
     'encKey' => env('PAYMENT_ENC_KEY'),
     'decKey' => env('PAYMENT_DEC_KEY'),
     'payUrl' => env('PAYMENT_URL'),
+    'transactionTrackingUrl' => env('PAYMENT_TRANSACTION_TRACKING_URL'),
     'merchantId' => env('PAYMENT_MERCHANT_ID'),
     'password' => env('PAYMENT_PAYMENT_PASSWORD')
 );
--- src/Atom.php
+++ src/Atom.php
@@ -4,18 +4,26 @@ namespace Rahulreghunath\Nttdatapay;
 
 class Atom
 {
+    private $password;
+    public $merchantId;
+
+    public function __construct()
+    {
+        $this->merchantId = config('nttdatapay.merchantId');
+        $this->password = config('nttdatapay.password');
+    }
     /**
      * Create payment authentication token
      *
-     * @param [type] $data
-     * @return [Integer] $atomTokenId
+     * @param Array $data
+     * @return Integer $atomTokenId
      */
     public function createTokenId($data)
     {
-        $api_url = config('nttdatapay.payUrl');
+        $data['payInstrument']['merchDetails']['merchId'] = $this->merchantId;
+        $data['payInstrument']['merchDetails']['password'] = $this->password;
 
-        $data['payInstrument']['merchDetails']['merchId'] = config('nttdatapay.merchantId');
-        $data['payInstrument']['merchDetails']['password'] = config('nttdatapay.password');
+        $url = config('nttdatapay.payUrl');
 
         $jsonData = json_encode($data);
 
@@ -23,7 +31,7 @@ class Atom
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $api_url,
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -38,6 +46,7 @@ class Atom
                 "Content-Type: application/x-www-form-urlencoded"
             ),
         ));
+
         $atomTokenId = null;
         $response = curl_exec($curl);
         $resp = json_decode($response, true);
@@ -50,10 +59,10 @@ class Atom
             $res = $this->decrypt($encresp);
             if (curl_errno($curl)) {
                 $error_msg = curl_error($curl);
-                echo "error = " . $error_msg;
+                // echo "error = " . $error_msg;
             }
             if (isset($error_msg)) {
-                echo "error = " . $error_msg;
+                // echo "error = " . $error_msg;
             }
             curl_close($curl);
 
@@ -69,13 +78,63 @@ class Atom
         return $atomTokenId;
     }
 
+
+
+    /**
+     * Track transaction status
+     *
+     * @param string $mer_txn
+     * @param string $amount
+     * @param string $date
+     * @return Array response array from API
+     */
+    public function transactionStatus($mer_txn, $amount, $date)
+    {
+        $data = "merchantid={$this->merchantId}&merchanttxnid={$mer_txn}&amt={$amount}&tdate={$date}";
+        $enc = $this->encrypt($data, 'sha1');
+
+        $url = config('nttdatapay.transactionTrackingUrl');
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_PORT => 443,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "login=" . $this->merchantId . "&encdata=" . $enc,
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/x-www-form-urlencoded"
+            ),
+            CURLOPT_USERAGENT => 'woo-commerce plugin',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        if ($err) {
+            $status = [''];
+        }
+
+        curl_close($curl);
+
+        return $this->decrypt($response, 'sha1');
+    }
+
     /**
      * Encrypt request data
      *
-     * @param [string] $data
-     * @return [String] encrypted data
+     * @param string $data
+     * @param string $digest_algo
+     * @return string encrypted data
      */
-    public function encrypt($data)
+
+    public function encrypt($data, $digest_algo = 'sha512')
     {
         $encRequestKey = config('nttdatapay.encKey');
         $method = "AES-256-CBC";
@@ -84,18 +143,19 @@ class Atom
         $IVbytes = join($chars);
         $salt1 = mb_convert_encoding($encRequestKey, "UTF-8"); //Encoding to UTF-8
         $key1 = mb_convert_encoding($encRequestKey, "UTF-8"); //Encoding to UTF-8
-        $hash = openssl_pbkdf2($key1, $salt1, '256', '65536', 'sha512');
+        $hash = openssl_pbkdf2($key1, $salt1, '256', '65536', $digest_algo);
         $encrypted = openssl_encrypt($data, $method, $hash, OPENSSL_RAW_DATA, $IVbytes);
-        return strtoupper(bin2hex($encrypted));
+        return bin2hex($encrypted);
     }
 
     /**
      * Decrypt request data
      *
-     * @param [string] $data
-     * @return [Array] decrypted data
+     * @param string $data
+     * @param string $digest_algo
+     * @return Array decrypted data
      */
-    public function decrypt($data)
+    public function decrypt($data, $digest_algo = 'sha512')
     {
         $decResponseKey = config('nttdatapay.decKey');
 
@@ -106,7 +166,7 @@ class Atom
         $IVbytes = join($chars);
         $salt1 = mb_convert_encoding($decResponseKey, "UTF-8"); //Encoding to UTF-8
         $key1 = mb_convert_encoding($decResponseKey, "UTF-8"); //Encoding to UTF-8
-        $hash = openssl_pbkdf2($key1, $salt1, '256', '65536', 'sha512');
+        $hash = openssl_pbkdf2($key1, $salt1, '256', '65536', $digest_algo);
         $decrypted = openssl_decrypt($dataEncypted, $method, $hash, OPENSSL_RAW_DATA, $IVbytes);
         return json_decode($decrypted, true);
     }
